095c105d9ba95bac52edbe8a30f6f9380af41f34
Claudia Ventresca <civentresca@gmail.com>
2015-05-08 23:28:06 -0400

    Added count for top 2 words that appear at random.

--- count-words-by-percentage.py
+++ count-words-by-percentage.py
@@ -3,21 +3,21 @@ from __future__ import print_function
 import re
 
 
-new_data = [{"text": "How are you?",
+new_data = [{"text": "What is the average life-span of a house fly?",
              "percent_correct": 0.8551724137931034},
-            {"text": "What is your name?",
+            {"text": "What is the capital of Washington state?",
              "percent_correct": 0.71379310344827586},
-            {"text": "Where do you live?",
+            {"text": "Where do fire ants live?",
              "percent_correct": 0.6779661016949152},
-            {"text": "How old are you?",
+            {"text": "How old was George Washington when he died?",
              "percent_correct": 0.5016949152542372},
-            {"text": "Do you drink coffee?",
+            {"text": "What is the number one coffee roaster in North America?",
              "percent_correct": 0.4932203389830508},
-            {"text": "Do you like salt?",
+            {"text": "Can penguins fly?",
              "percent_correct": 0.3217391304347826},
-            {"text": "What is the thing you do best?",
+            {"text": "What is the capital of New York state?",
              "percent_correct": 0.2101449275362319},
-            {"text": "Is she your sibling?",
+            {"text": "Who composed the American National Anthem?",
              "percent_correct": 0.1246376811594203}]
 
 def get_clean_data(r):
@@ -33,65 +33,57 @@ def get_clean_data(r):
         clean_r.append(i.lower())
     return clean_r
 
-def count_words(clean_above_fortynine, clean_below_fifty):
+def count_words(words):
     '''
     Counts how many times a value is being passed from a list object
     '''
+    print(words)
     # Used to keep a unique list of all the values passed via new_data
-    above_fortynine = []
-    below_fifty = []
+    clean_words = []
     # keeps a dict of all the values passed via new_data and the
     # number of times each value has been seen throughout the iterations
-    above_fortynine_count = {}
-    below_fifty_count = {}
+    clean_words_count = {}
     # Process data for values associated with a score above 49 precentile
-    for i in clean_above_fortynine:
+    for i in words:
         #print(i) # delete at will
         i = i.split()
         #print(i)
         # If the values has already been processed once, up the count by 1
         for s in i:
             #print(s)
-            if s in above_fortynine:
-                above_fortynine_count[s] = above_fortynine_count[s] + 1
+            if s in clean_words:
+                clean_words_count[s] = clean_words_count[s] + 1
                 # delete at will
                 #print(s + " is already in here; up-ing the count!")
                 #print(above_fortynine_count) # delete at will
             # If this is the first time seeing the value, add it to the dict,
             # and initate its count to 1
             else:
-                above_fortynine.append(s)
-                above_fortynine_count[s] = 1
-                #print(above_fortynine_count) # delete at will
-    print("Word Count Above FORTY NINE %: ")
-    print(above_fortynine_count)
-    print(max(above_fortynine_count.iterkeys(), key=(lambda key: above_fortynine_count[key])))
-
-    # Process data for values associated with a score below 50 precentile
-    for i in clean_below_fifty:
-        #print(i) # delete at will
-        i = i.split()
-        #print(i)
-        # If the values has already been processed once, up the count by 1
-        for s in i:
-            #print(s)
-            if s in below_fifty:
-                below_fifty_count[s] = below_fifty_count[s] + 1
-                # delete at will
-                #print(s + " is already in here; up-ing the count!")
-                #print(below_fifty_count) # delete at will
-            # If this is the first time seeing the value, add it to the dict,
-            # and initate its count to 1
-            else:
-                below_fifty.append(s)
-                below_fifty_count[s] = 1
-                # delete at will
-                #print(below_fifty_count)
-    print("Word Count Below FIFTY %: ")
-    print(below_fifty_count)
-    print(max(below_fifty_count.iterkeys(), key=(lambda key: below_fifty_count[key])))
+                clean_words.append(s)
+                clean_words_count[s] = 1
+    print(clean_words_count) # delete at will
+    # pull MAX but make sure it is greater than 1
+    ii = (max(clean_words_count.iterkeys(), \
+              key=(lambda key: clean_words_count[key])))
+    if clean_words_count[ii] > 1:
+        print(ii, clean_words_count[ii])
+        del clean_words_count[ii]
+        print(clean_words_count)
+        # pull MAX but make sure it is greater than 1
+        ii = (max(clean_words_count.iterkeys(), \
+              key=(lambda key: clean_words_count[key])))
+        if clean_words_count[ii] > 1:
+            print(ii, clean_words_count[ii])
+            del clean_words_count[ii]
+            print(clean_words_count)
+        else:
+            print("Value found was NOT GREATER THAN 1!")
+    else:
+        print("Value found was NOT GREATER THAN 1!")
 
 
+top_above_fortynine = {}
+top_below_fifty = {}
 above_fortynine = []
 below_fifty = []
 # Show us what's in the data-set - delete at will
@@ -106,4 +98,6 @@ for i in new_data:
 #print(below_fifty)
 clean_above_fortynine = get_clean_data(above_fortynine)
 clean_below_fifty = get_clean_data(below_fifty)
-count_words(clean_above_fortynine, clean_below_fifty)
+
+top_above_fortynine = count_words(clean_above_fortynine)
+top_below_fifty = count_words(clean_below_fifty)
