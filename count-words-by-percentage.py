f871ed4f3f37d83e6494fef2f9836469b8ff7899
Claudia Ventresca <civentresca@gmail.com>
2015-05-09 10:19:56 -0400

    (1) Modified sample data-set to instigate a usecase where no legitimate words were found more than once (2) Modified coded so that it only returns ALL WORDS with TOP-TWO HIGHEST COUNTS.

--- count-words-by-percentage.py
+++ count-words-by-percentage.py
@@ -5,24 +5,25 @@ import re
 
 new_data = [{"text": "What is the average life-span of a house fly?",
              "percent_correct": 0.8551724137931034},
-            {"text": "What is the capital of Washington state?",
+            {"text": "What is the capital of any state?",
              "percent_correct": 0.71379310344827586},
             {"text": "Where do fire ants live?",
              "percent_correct": 0.6779661016949152},
-            {"text": "How old was George Washington when he died?",
+            {"text": "How old was the first president when he died?",
              "percent_correct": 0.5016949152542372},
             {"text": "What is the number one coffee roaster in North America?",
              "percent_correct": 0.4932203389830508},
             {"text": "Can penguins fly?",
              "percent_correct": 0.3217391304347826},
-            {"text": "What is the capital of New York state?",
+            {"text": "What is the capital of New York York York state?",
              "percent_correct": 0.2101449275362319},
-            {"text": "Who composed the American National Anthem?",
+            {"text": "Who composed the American National Anthem Anthem?",
              "percent_correct": 0.1246376811594203}]
+
 
 def get_clean_data(r):
     '''
-    Process data to clean up leading and trailing spaces and applies a regex to
+    Processes data to clean up leading and trailing spaces and applies a regex to
     eliminate unwanted characters in this case ( ) { } - < > and empty spaces
     in-between words.  More characters can be added as needed.
     '''
@@ -33,20 +34,68 @@ def get_clean_data(r):
         clean_r.append(i.lower())
     return clean_r
 
+def get_max_count(clean_words_count):
+    '''
+    Get top-two random words with the highest number of occurances, as long as
+    said words are not included in the 'exclusions' list
+    '''
+    # Add/Delete to/from exclusions list at will
+    exclusions = ["a", "an", "the", "and", "but", "or", "for", \
+                  "nor", "etc", "on", "at", "to", "from", "by", \
+                  "is", "what", "of"]
+    top_count = {}
+    # capture passed values and shorten
+    # used to mutate dict outside of the iteration
+    clean_words_count_copy = {}
+    clean_words_count_copy = dict.copy(clean_words_count)
+    ii_count = 0
+    # pull MAX but make sure that max is greater than 1
+    # and run x times as long as clean_words_count[ii] > 1 and two top values
+    # have not been found yet.
+    # Will iterate through every value in the collection and aler if the value
+    # associated with the key is not greater than 1 and will igonore any values
+    # which are matched successfully against the values in the 'exclusions' list
+    # TODO: Modify the code so that it doesnt alert for every single value,
+    #       which is not greater than one, instead it should alert once and show
+    #       all values which were not in the 'exclusions' list but had a value
+    #       of only 1 <<< this may be over-kill for larger data sets, but cool
+    #       to show in this example
+    for x in clean_words_count:
+        if ii_count < 2:
+           # print(ii_count)
+            ii = (max(clean_words_count_copy.iterkeys(), \
+                      key=(lambda key: clean_words_count_copy[key])))
+            if clean_words_count_copy[ii] > 1:
+                #print(ii, clean_words_count_copy[ii])
+                if ii not in exclusions:
+                    top_count[ii] = clean_words_count_copy[ii]
+                    #print(top_count)
+                    del clean_words_count_copy[ii]
+                    #print(clean_words_count)
+                    # pull MAX but make sure it is greater than 1
+                    ii_count = ii_count + 1
+                    #print(ii_count)
+                else:
+                    del clean_words_count_copy[ii]
+            else:
+                print("Value found was NOT GREATER THAN 1!")
+    print("These are the words with the highest top two counts:")
+    print(top_count)
+    print(" ")
+    return top_count
+
 def count_words(words):
     '''
     Counts how many times a value is being passed from a list object
     '''
+    print("These are the raw values passed in, after a clean-up:")
     print(words)
-    exclusions = ["a", "an", "the", "and", "but", "or", "for", \
-                  "nor", "etc", "on", "at", "to", "from", "by"]
+    print(" ")
     # Used to keep a unique list of all the values passed via new_data
     clean_words = []
     # keeps a dict of all the values passed via new_data and the
     # number of times each value has been seen throughout the iterations
     clean_words_count = {}
-    # Process data for values associated with a score above 49 precentile
-    top_count = {}
     for i in words:
         #print(i) # delete at will
         i = i.split()
@@ -58,41 +107,20 @@ def count_words(words):
                 clean_words_count[s] = clean_words_count[s] + 1
                 # delete at will
                 #print(s + " is already in here; up-ing the count!")
-                #print(above_fortynine_count) # delete at will
-            # If this is the first time seeing the value, add it to the dict,
-            # and initate its count to 1
             else:
+                # If this is the first time seeing the value, add it to the dict,
+                # and initate its count to 1
                 clean_words.append(s)
                 clean_words_count[s] = 1
+    print("These are the same values with associated counts per word:")
     print(clean_words_count) # delete at will
-    # pull MAX but make sure it is greater than 1
-    # TODO: Turn this into a function that can run x times
-    # as long as clean_words_count[ii] > 1 and ignore all
-    # values in the "exclusions" list
-    ii = (max(clean_words_count.iterkeys(), \
-              key=(lambda key: clean_words_count[key])))
-    if clean_words_count[ii] > 1:
-        #print(ii, clean_words_count[ii])
-        top_count[ii] = clean_words_count[ii]
-        print(top_count)
-        del clean_words_count[ii]
-        print(clean_words_count)
-        # pull MAX but make sure it is greater than 1
-        ii = (max(clean_words_count.iterkeys(), \
-              key=(lambda key: clean_words_count[key])))
-        if clean_words_count[ii] > 1:
-            #print(ii, clean_words_count[ii])
-            top_count[ii] = clean_words_count[ii]
-            print(top_count)
-            del clean_words_count[ii]
-            print(clean_words_count)
-        else:
-            print("Value found was NOT GREATER THAN 1!")
-    else:
-        print("Value found was NOT GREATER THAN 1!")
+    print(" ")
+    top_count = get_max_count(clean_words_count)
     return top_count
 
-
+###
+#TODO: Add description here ...
+###
 above_fortynine = []
 below_fifty = []
 top_above_fortynine = {}
@@ -112,8 +140,18 @@ clean_below_fifty = get_clean_data(below_fifty)
 
 top_above_fortynine = count_words(clean_above_fortynine)
 top_below_fifty = count_words(clean_below_fifty)
-print("The words with the highest count in the ABOVE FORTININE percentile are: ",\
-       top_above_fortynine)
-print("The words with the highest count in the BELOW FIFTY percentile are:", \
-       top_below_fifty)
+if bool(top_above_fortynine) == True:
+    print("The words with the highest count in the ABOVE FORTY-NINE percentile \
+           are: ", top_above_fortynine)
+else:
+    print("All The words with the highest count in the BELOW FIFTY \
+           percentile were in the exclusions list. All other values appear \
+           exactly once.  There is nothing to return.")
 
+if bool(top_below_fifty) == True:
+    print("The words with the highest count in the BELOW FIFTY percentile \
+           are:", top_below_fifty)
+else:
+    print("All The words with the highest count in the BELOW FIFTY \
+           percentile were in the exclusions list. All other values appear \
+           exactly once.  There is nothing to return.")
